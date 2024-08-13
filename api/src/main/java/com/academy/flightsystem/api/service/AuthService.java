4ae7504e5f72a7bb956731300c80aedcb47043a5
Alen Paunov <alen.paunov@gmail.com>
2024-08-13 21:13:43 +0300

    last second fixes

--- api/src/main/java/com/academy/flightsystem/api/service/AuthService.java
+++ api/src/main/java/com/academy/flightsystem/api/service/AuthService.java
@@ -4,6 +4,7 @@ import com.academy.flightsystem.api.model.UserInfo;
 import com.academy.flightsystem.api.model.dto.LoginUserDto;
 import com.academy.flightsystem.api.model.dto.RegisterUserDto;
 import com.academy.flightsystem.api.repository.UserRepository;
+import com.academy.flightsystem.api.security.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -24,6 +25,9 @@ public class AuthService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private JwtService jwtService;
+
     public UserInfo register(RegisterUserDto userDto){
         UserInfo userInfo = new UserInfo();
         userInfo.setUsername(userDto.getUsername());
@@ -32,9 +36,15 @@ public class AuthService {
         return userRepository.save(userInfo);
     }
 
-    public Optional<UserInfo> login(LoginUserDto userDto){
-        authenticationManager.authenticate( new UsernamePasswordAuthenticationToken(userDto.getUsername(),userDto.getPassword()));
-        return userRepository.findByUsername(userDto.getUsername());
+    // LoginResponse(token, user)
+
+    public String login(LoginUserDto userDto){
+
+
+        var token = jwtService.generateToken(authenticationManager.authenticate( new UsernamePasswordAuthenticationToken(userDto.getUsername(),userDto.getPassword())).getPrincipal().toString());
+        var user = userRepository.findByUsername(userDto.getUsername()).orElseThrow();
+        return  token +" | " +  user.getUsername();
+
     }
 
 }
